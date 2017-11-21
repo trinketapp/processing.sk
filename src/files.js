beab349f55282832f7d20faecc3dcce48b362c23
Albert-Jan Nijburg <albertjan@skimlinks.com>
2017-11-21 08:28:32 +0000

    open isnt even provided separately by processing.py

--- src/files.js
+++ src/files.js
@@ -11,7 +11,6 @@ export default {
         { "filename": str }
     ]),
     createInput: notImplemented,
-    open: notImplemented,
     selectFolder: notImplemented,
     selectInput: notImplemented
 };
