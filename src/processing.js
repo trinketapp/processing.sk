b8ab987480d989d8fa927e812c0576dd0fe8668a
Albert-Jan Nijburg <albertjan@trinket.io>
2018-09-10 23:06:17 +0100

    firstFrameCallback and sketch termination without a draw function

--- src/processing.js
+++ src/processing.js
@@ -61,7 +61,9 @@ let seenCanvas = null;
 let doubleBuffered = true;
 let eventPred = () => true;
 
-export function init(path, suspensionHandler, breakHandler, eventPredicate) {
+let firstFrameFunc = null;
+
+export function init(path, suspensionHandler, breakHandler, eventPredicate, firstFrameCallback) {
     suspHandler = suspensionHandler;
     if (breakHandler !== undefined && typeof breakHandler !== "function") {
         throw new Error("breakHandler must be a function if anything");
@@ -79,6 +81,10 @@ export function init(path, suspensionHandler, breakHandler, eventPredicate) {
 
     if (typeof eventPredicate === "function") {
         eventPred = eventPredicate;
+    }
+
+    if (typeof firstFrameCallback === "function") {
+        firstFrameFunc = firstFrameCallback;
     }
 }
 
@@ -139,6 +145,7 @@ export function main() {
         let finish = null;
         let canvas = null;
         let parentNode = null;
+        let firstFrame = firstFrameFunc !== null;
 
         susp.resume = function() {
             if (susp.data["error"]) {
@@ -169,22 +176,35 @@ export function main() {
                 };
             }
 
-            if (Sk.globals["draw"]) {
-                proc.draw = function () {
-                    // call the break handler every draw so the processing.sk is stoppable.
-                    if (bHandler) {
-                        try {
-                            bHandler();
-                        } catch (e) {
-                            throwAndExit(e);
-                        }
+            proc.draw = function () {
+                // call the break handler every draw so the processing.sk is stoppable.
+                if (bHandler) {
+                    try {
+                        bHandler();
+                    } catch (e) {
+                        throwAndExit(e);
                     }
+                }
 
+                if (Sk.globals["draw"]) {
                     return asyncToPromise(
                         () => callsimOrSuspend(Sk.globals["draw"]), suspHandler
-                    );
-                };
-            }
+                    ).then(res => {
+                        if (firstFrame) {
+                            firstFrameFunc();
+                            firstFrame = false;
+                        }
+
+                        return res;
+                    });
+                } else {
+                    if (firstFrame) {
+                        firstFrameFunc();
+                        firstFrame = false;
+                    }
+                    proc.exit();
+                }
+            };
 
             var callBacks = [
                 "mouseMoved", "mouseClicked", "mouseDragged", "mouseMoved", "mouseOut",
